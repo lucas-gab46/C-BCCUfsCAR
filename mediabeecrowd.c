e415906790f483398f49c7dd8e9b9b988d358c30
lucas-gab46 <121887128+lucas-gab46@users.noreply.github.com>
2024-07-31 13:59:38 -0300

    Add files via upload

---
+++ mediabeecrowd.c
@@ -0,0 +1,16 @@
+int main(){
+double a,b;
+double MEDIA, MEDIA2;
+// Medias
+MEDIA = (a = 3.5); 
+MEDIA = (b = 7.5);
+
+// Media Ponderada  
+    MEDIA = (a * 3.5 + b * 7.5) / (3.5 + 7.5);
+
+
+scanf(".5%f\n, &a, &b");
+// Resultado da Media 
+    printf("MEDIA = %.2f\n", MEDIA);
+
+ }
